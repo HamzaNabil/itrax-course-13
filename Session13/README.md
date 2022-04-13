a32591cd903119b30588286b24ab4912ac3c5585
HamzaNabil <hamzanabil913@gmail.com>
2022-04-14 00:14:56 +0200

    add JS Part 1

---
+++ Session13/README.md
@@ -0,0 +1,21 @@
+# JavaScript Part 1
+
+# Content :
+
+- Introduction
+
+- Where To Write
+
+- Syntax
+
+- Output [ document write | getElement | console log]
+
+- Statement
+
+- Comments
+
+- Variable
+
+- Operators
+
+- Decrement | Increment
---
+++ Session13/js/script.js
@@ -0,0 +1,67 @@
+//
+// document.write(1 + 1);
+// console.log("fwefwef");
+
+// document.getElementById("demo").innerHTML = "Js1";
+
+// <button></button>
+// document.getElementById("btn").onclick = function () {
+//   document.getElementById("demo").innerHTML = "Hello Js";
+// };
+
+// ewfwefwefwefwef
+
+/*
+
+*/
+// statment
+// variables
+// var button = document.getElementById("btn");
+
+// button.onclick = function () {
+//   button.innerHTML = "Test";
+// };
+
+// Data Types
+var x = 1; // number
+var y = "test"; // string
+var z = true; // boolean
+// console.log(y + z);
+// console.log("hello" + y + "hamada");
+
+document.getElementById("demo").innerHTML = "<p>" + y + "</p>";
+// "<p> tesr </p>"
+// document.getElementById("demo").innerHTML = "<p> wefwefwef</p> ";
+// document.getElementById("demo").innerText = "wqfdqwfwefewf";
+
+// Operators
+//  +  - /  * % ++ --
+// console.log(9 % 2);
+
+// ++ increment
+// -- decrement
+
+var n = 5;
+n++; // 6
+++n; // 7
+n--; // 6
+++n; // 7
+n++; // 8
+console.log(n); // 8
+
+console.log(n++); // 8
+// 9
+n++; // 10
+console.log(++n); // 11
+console.log(n--); // 11
+// 10
+console.log(--n); // 9
+console.log(n); // 9
+// loop
+var t = 2;
+
+t = t + 1;
+
+t += 1;
+
+t++;
